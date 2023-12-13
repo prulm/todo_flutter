3431d8693570ef519da5117e1abb7cf8aca34947
Yared F <jaredyared83@gmail.com>
2023-12-13 09:02:51 +0300

    basic structure

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,33 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("Notes"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("The broken wings"),
+              subtitle: Text("10/24 09:55"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
