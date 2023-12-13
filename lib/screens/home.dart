acf1f90cffcfe30a0e036d1d18314f08d87b5fe2
Yared F <jaredyared83@gmail.com>
2023-12-13 09:55:43 +0300

    notes retrieve request set up

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:todo_flutter/models/note.dart';
+import 'package:todo_flutter/services/api_service.dart';
+import 'package:todo_flutter/utils/constants.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,16 +14,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Client client = http.Client();
+  List<Note> notes = [];
+
+  @override
+  void initState() {
+    _retrieveNotes();
+    super.initState();
+  }
+
+  _retrieveNotes() async {
+    notes = [];
+
+    List response = json.decode((await client.get(retrieveUrl)).body);
+
+    response.forEach((element) {
+      notes.add(Note.fromMap(element));
+    });
+    setState(() {});
+  }
+
+  void _addNote() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
         elevation: 0,
-        title: Text("Notes"),
+        title: Text(
+          "Notes",
+          style: TextStyle(color: Colors.white),
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.more_vert),
+            icon: Icon(Icons.more_vert, color: Colors.white),
             onPressed: () {},
           ),
         ],
@@ -34,7 +66,7 @@ class _HomeState extends State<Home> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _addNote,
         child: Icon(Icons.add),
       ),
     );
---
+++ lib/utils/constants.dart
@@ -0,0 +1,1 @@
+var retrieveUrl = Uri.https('localhost', 'note/load/');
